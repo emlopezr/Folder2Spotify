a51e58f66701b8f8595f32d863ddc29a2ae4fb44
Emmanuel López Rodríguez <contact@emlopezr.com>
2024-02-25 22:30:16 -0500

    Refactor code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from modules.select_language import select_language
-from modules.dump_names import dump_song_names
-from modules.generate_playlist import generate_spotify_playlist
-import messages
+from utils.select_language import select_language
+from modules.dump_song_names import dump_song_names
+from modules.generate_spotify_playlist import generate_spotify_playlist
+import utils.messages as messages
 
 def main():
     language = select_language()
@@ -11,7 +11,7 @@ def main():
     dump_file = dump_song_names(language)
     generate_spotify_playlist(dump_file, language)
 
-    print(messages.THANKS_MESSAGE[language])
+    print('\n' + messages.THANKS_MESSAGE[language])
     print(messages.SIGNATURE_MESSAGE)
 
 if __name__ == "__main__": main()
